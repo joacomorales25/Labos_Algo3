fc34edadc1dd228e83022137aa6b46b4393a2f3f
Joaquin Morales <73201854+joacomorales25@users.noreply.github.com>
2023-05-19 20:29:24 -0300

    ej1 solucionado

--- LaboAlgo3/TP2/ej1.cpp
+++ LaboAlgo3/TP2/ej1.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <utility>
 #include <algorithm>
+#include <set>
 #include <iomanip>
 #include <map>
 using namespace std;
@@ -10,44 +11,41 @@ using namespace std;
 
 enum estados{NO_VISITADO, VISITADO, EN_PROCESO};
 vector<int> estado;
-//DETECCIÓN DE PUENTES
 int componente = 0;
 vector<int> elems;
 vector<bool> visitados;
-map<pair<int, int>, bool> puentes;
+vector<int> puentes;
 
-void dfs_visit(const vector<vector<int>> &ady, int u, vector<int> &parent, vector<int> &discTime, int &time, vector<int> &color, vector<bool> &bridgeParent){
+
+
+void dfs_visit(const vector<vector<int>> &ady, int u, vector<int> &parent, vector<int> &discTime, int &time, vector<int> &color){
     time = time + 1;
     discTime[u] = time;
     color[u] = 1;
     for (auto v : ady[u]){
         if (color[v] == 0){
             parent[v] = u;
-            dfs_visit(ady, v, parent, discTime, time, color, bridgeParent);
+            dfs_visit(ady, v, parent, discTime, time, color);
         }
 
     }
     color[u] = 2;
 }
 
-void DFS(const vector<vector<int>> &ady, vector<int> &parent, vector<int> &discTime, vector<int> &color, vector<bool> &bridgeParent){
+void DFS(const vector<vector<int>> &ady, vector<int> &parent, vector<int> &discTime, vector<int> &color){
     int time = 0;
-    for (int u = 1; u < ady.size(); u++){
-        if (color[u] == 0){
-            parent[u] = u;
-            componente++;
-            dfs_visit(ady, u, parent, discTime, time, color, bridgeParent);
-        }
-    }
+    parent[1] = 1;
+    dfs_visit(ady, 1, parent, discTime, time, color);
+        
 }
 
 
 
-int detectarPuentes(const std::vector<std::vector<int>> &ady, int u, std::vector<int> &parent, std::vector<int> &discTime, std::vector<bool> &bridgeParent){
+int detectarPuentes(const vector<vector<int>> &ady, int u, vector<int> &parent, vector<int> &discTime){
     int cantidad = 0;
     for (auto v : ady[u]){
         if (parent[v] == u){    //por cada tree-edge hacia abajo le sumamos la cantidad de back-edges que lo cubren.
-            cantidad += detectarPuentes(ady, v, parent, discTime, bridgeParent);
+            cantidad += detectarPuentes(ady, v, parent, discTime);
         } else {
             if (discTime[u] > discTime[v] && parent[u] != v){   //sumamos los back-edges que salen de u y van hacia arriba.
                 cantidad++;
@@ -57,10 +55,10 @@ int detectarPuentes(const std::vector<std::vector<int>> &ady, int u, std::vector
             }
         }
     }
-    if (cantidad == 0 && parent[u] != u)    //si no hay back-edges que cubran la tree-edge entre u y su padre, entonces es puente. Si u es raíz, no vale.
-        puentes[make_pair(u, parent[u])] = true;
-        puentes[make_pair(parent[u], u)] = true;
-
+    if(cantidad == 0 && parent[u] != u){  //si no hay back-edges que cubran la tree-edge entre u y su padre, entonces es puente. Si u es raíz, no vale.
+        puentes[u] = parent[u];
+    }
+    
     return cantidad;
 }
 
@@ -68,7 +66,7 @@ void dfs(int u, Grafo& g){
     estado[u] = EN_PROCESO;
     elems[componente]++;
     for(int v : g[u]){
-        if(estado[v] == NO_VISITADO && (!puentes[make_pair(u, v)] || !puentes[make_pair(v, u)])){
+        if(estado[v] == NO_VISITADO && !(puentes[v] == u || puentes[u] == v)){
             dfs(v, g);
         }
     }
@@ -92,20 +90,17 @@ int main(){
     vector<int> parent(nodos+1, -1);
     vector<int> discTime(nodos+1, -1);
     vector<int> color(nodos+1, 0);
-    vector<bool> bridgeParent(nodos+1, false);
     elems = vector<int>(nodos+1, 0);
-    DFS(g, parent, discTime, color, bridgeParent);      //corremos DFS para obtener la info necesaria para detectar puentes
+    puentes = vector<int>(nodos+1, -1);
+    DFS(g, parent, discTime, color);      //corremos DFS para obtener la info necesaria para detectar puentes
 
-    for(int i = 1; i <= nodos; i++){
-        detectarPuentes(g, i, parent, discTime, bridgeParent);
-    }
-
+    detectarPuentes(g, 1, parent, discTime);
     
         //corremos DFS para obtener la info necesaria para detectar puentes
-    int componentes = 0;
     estado = vector<int>(nodos+1, NO_VISITADO);
     for(int i = 1; i <= nodos; i++){
         if(estado[i] == NO_VISITADO){
+            componente++;
             dfs(i, g);
         }
     }
@@ -113,15 +108,14 @@ int main(){
 
     for(int i = 0; i < elems.size(); i++){
         
-        formas_de_ganar += (elems[i]*(elems[i]-1))/2;
+        formas_de_ganar += ((long long)elems[i]*(long long)(elems[i]-1))/2;
         
     }
-    
-    long long formas_de_jugar = (nodos*(nodos-1))/2;
+
+    long long formas_de_jugar = ((long long)nodos*(long long)(nodos-1))/2;
     double prob_ganar = (double)formas_de_ganar/(double)formas_de_jugar;
     cout << fixed << setprecision(5) << 1-prob_ganar << endl;
 }
-
 
 
 
